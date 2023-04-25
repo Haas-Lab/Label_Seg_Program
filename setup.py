c0e6e2b5fc4495b7620f3b1e060a74183e029071
Jason Fung <jasonfung@Jasons-MacBook-Air-5.local>
2023-04-25 11:38:21 -0700

    added setup for dl'ing ML models

---
+++ setup.py
@@ -0,0 +1,12 @@
+import gdown
+
+# get other modules from pip install
+
+
+# get machine learning models 
+url = "https://drive.google.com/drive/folders/1M2KH35yENuwZMBd4AQ7HC17oP-DYlked?usp=share_link"
+gdown.download_folder(url, quiet=False, use_cookies=False)
+
+
+
+
